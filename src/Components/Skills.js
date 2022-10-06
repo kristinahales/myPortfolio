535029730da5b574a5a1b7f212ddcc8785dc4e27
kristinahales <kristinakfriel@gmail.com>
2022-10-06 12:27:34 -0600

    small wording change on skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,7 +5,7 @@ class Skills extends React.Component {
     render() {
       return   <div className='skills-main-container'>
       <div className='skills-header'>Skills & Experience</div>
-      <p className='skills-details'>Since beginning my journey as a web developer over three years ago, I have connected to multiple API's one of which was Azure Devops. I've created successful responsive web pages that are easy to use and built with best practices. The main areas of my expertise is:</p>
+      <p className='skills-details'>Since beginning my journey as a web developer over three years ago, I have connected to multiple API's, one of which was Azure Devops. I've created successful responsive web pages that are easy to use and built with best practices. The main areas of my expertise is:</p>
         <li>Front-end development</li>
         <li>HTML</li>
         <li>CSS</li>
