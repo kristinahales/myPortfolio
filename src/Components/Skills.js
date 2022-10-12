527b9ee08d67684813b3ff691f26a065d71bf0de
kristinahales <kristinakfriel@gmail.com>
2022-10-12 16:13:45 -0600

    updated skills page with project

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -12,7 +12,14 @@ class Skills extends React.Component {
         <li>JS</li>
         <li>SQL</li>
         <li>React</li>
-        <p className='skills-details'>I am also a firm believer that it is important to keep learning. I recently finished the course <a className='udemy' href="https://www.udemy.com/course/the-complete-sql-bootcamp/">The Complete SQL Bootcamp 2022</a> and have started the course <a className='udemy' href="https://www.udemy.com/course/java-programming-tutorial-for-beginners/">Java Programming for Complete Beginners</a>.</p>
+
+        <p className='skills-details'>I've been having fun creating a little React.js application. Using Github pages, I hosted a small API. I deployed my application using Netlify. Some new NPM packages I used were react-burger-menu and react-modal. Check out my code <a className='udemy' href="https://github.com/kristinahales/RecipeApp">here</a>.</p>
+        <div className='skills-image-container'>
+          <img className='skills-image' src={require('./Images/desserts.png')} />
+          <img className='skills-image' src={require('./Images/dessertinfo.png')} />
+          <img className='skills-image' src={require('./Images/menu.png')} />
+        </div>
+        <p className='skills-details'>I am also a firm believer that it is important to keep learning. Recently I finished the course <a className='udemy' href="https://www.udemy.com/course/the-complete-sql-bootcamp/">The Complete SQL Bootcamp 2022</a> and have started the course <a className='udemy' href="https://www.udemy.com/course/java-programming-tutorial-for-beginners/">Java Programming for Complete Beginners</a>.</p>
         <p className='skills-details'>Visit my <a href="https://www.linkedin.com/in/kristina-h-873311184/">LinkedIn</a> profile for more details.</p>
     </div>
     }
