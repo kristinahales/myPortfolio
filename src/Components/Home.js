e4d15bce908420c4daae2e0c514936ec65f53af7
kristinahales <kristinakfriel@gmail.com>
2022-10-12 15:51:49 -0600

    updated skills page briefly

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
     render() {
       return   <div className='main-container'>
       <div className='home-header'>Hi. <br/> I'm <span className='first-name'>Kristina</span>,<br/> web developer.</div>
-      <img className='home-image' src={require('./Images/MyCollage.png')} />
+      <img className='home-image' src={require('./Images/portfolioCollage.png')} />
       <p></p>
     </div>
     }
